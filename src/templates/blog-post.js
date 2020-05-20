9d86ee34c3bfa020fc0f21d8a5acdfa87f098416
W <painterwil@gmail.com>
2020-05-19 21:24:45 -0400

    blogff

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,6 +10,8 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata.title
   const { previous, next } = pageContext
+   
+  const baseUrl = 'https://blog.thetechcopywriter.com/'
 
   return (
     <Layout location={location} title={siteTitle}>
@@ -19,6 +21,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
       />
       <article>
         <header>
+
           <h1
             style={{
               marginTop: rhythm(1),
@@ -74,6 +77,37 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
           </li>
         </ul>
       </nav>
+
+
+
+      <h3 className="text-center">Share this post</h3>
+     <div className="text-center.social-share-links"> 
+      <ul>
+
+        <li style={{display:'inline'}}><a href={
+          'https://www.facebook.com/sharer/sharer.php?u=' +
+           baseUrl + pageContext.slug}
+           className="facebook" target="_blank" rel="noopener noreferrer">
+          
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z"/></svg>
+          &nbsp;&nbsp; &nbsp;&nbsp;
+          
+
+          </a></li>
+
+          <li><a href={'https://www.linkedin.com/sharing/share-offsite/?url=' + baseUrl + pageContext.slug} className="linkedin" target="_blank" rel="noopener noreferrer">
+
+          
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+         
+          <script src="https://platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
+          <script type="IN/Share" data-url="https://www.linkedin.com"></script>
+          
+           </a></li>
+      </ul>
+     </div>
+
+
     </Layout>
   )
 }
